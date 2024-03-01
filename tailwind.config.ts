c022b5ddfd2acb103b19d1185b334455f8ea9c8f
Reza <r.bahar91@gmail.com>
2024-03-01 14:15:05 +0530

    added prettier and tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,12 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
-  theme: {
-    
-  },
-  plugins: [],
-};
-export default config;
+  theme: {},
+  plugins: [require('@tailwindcss/forms')]
+}
+export default config
